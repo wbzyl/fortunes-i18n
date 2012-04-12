e8ac95477e867dc5e78cdfa507492bb4eee31adb
Wlodek Bzyl <matwb@univ.gda.pl>
2012-04-12 20:01:56 +0200

    bootstrapped application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Polski::Application.config.secret_token = 'b7bcced672aef2a2eee18c25ed5a87a2652562f0e89349844094ac198cdfcbbff1217324e271bbe7e4005bad4e607bfdd92b3da9297e2c349ac3bb4501142e2a'
