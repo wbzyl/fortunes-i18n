d0706afb573e8836f36c0d187d72a5cd3bac602a
Wlodek Bzyl <matwb@univ.gda.pl>
2012-04-13 14:22:10 +0200

    poprawki: edycja -> edytuj

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120412181812) do
+ActiveRecord::Schema.define(:version => 20120412184049) do
 
   create_table "fortunes", :force => true do |t|
     t.text     "quotation"
